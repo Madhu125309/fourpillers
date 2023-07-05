a3c31dcdbd2a9ec4a90fb8026a381217f6e19c5b
7997259422 <madhureddy85767@gmail.com>
2023-07-05 18:16:51 +0530

    hey

---
+++ demo3.java
@@ -0,0 +1,11 @@
+public class demo3 {
+
+    public static void main(String[] args) {
+        StringBuffer s1 = new StringBuffer("madhu");
+        StringBuilder s2 = new StringBuilder("yash");
+        s1.append("sudhan");
+        s2.append("reddy");
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
---
+++ demo4.java
@@ -0,0 +1,10 @@
+class demo4 {
+    public static void main(String[] args) {
+        String s1 = new String("madhu");
+        String s2 = new String("yash");
+        s1.concat("sudhan");
+        s2.concat("reddy");
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
